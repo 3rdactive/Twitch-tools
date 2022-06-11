c13a4f1e632fe95d3ceb12dce4ca25d2cd206ec1
clickinggames <68467355+3rdactive@users.noreply.github.com>
2022-06-11 11:23:35 +0300

    TwitchPet directory

---
+++ Twitch-Pet/README.md
@@ -0,0 +1,3 @@
+this module is still in progress!
+
+i advise to not use this tool while its still in beta!
---
+++ Twitch-Pet/config.js
@@ -0,0 +1,10 @@
+// insert your twitch auth token here. MAKE SURE U DONT SHARE THIS WITH ANYONE YOU DONT TRUST!
+// you can generate a twitch auth token here: https://twitchapps.com/tmi/
+// how a token should look like: "oauth:341234432112344321123443211234"
+var Auth = "Your auth here!"
+
+
+//insert the username of the account you used to generate the auth token!
+var UsId = "Your username here!"
+
+
